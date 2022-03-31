77ccebf89a4cfb6aa89369abf82be2e2d6649624
Karthik <jeefkarthik036@gmail.com>
2022-03-31 08:49:44 +0530

    init variables

---
+++ GamblingSImulator.java
@@ -0,0 +1,14 @@
+package com.bridge;
+
+public class GamblingSImulator {
+	private static final int Amt = 100;
+	private static final int bet =1;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Gambler simulation");
+		
+
+	}
+
+}
