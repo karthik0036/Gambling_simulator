7ea5310b86a47647cbb37a09b848bd9982ca7785
Karthik <jeefkarthik036@gmail.com>
2022-03-31 09:20:05 +0530

    added-UC2 Pgm

--- GamblingSImulator.java
+++ GamblingSImulator.java
@@ -3,12 +3,35 @@ package com.bridge;
 public class GamblingSImulator {
 	private static final int Amt = 100;
 	private static final int bet =1;
+	public int stake =0;
+	
+	
+	public void game() {
+		double game = Math.floor(Math.random()*10)%2;
+		
+		if (bet == game ) {
+			System.out.println("won 1 dollar");
+			stake++;
+		}
+		else {
+			System.out.println("lost 1 dollar");
+			stake--;
+		}
+		System.out.println("stake :"+ stake);
+	}
+		
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Gambler simulation");
+		GamblingSImulator a = new GamblingSImulator();
+		a.game();
+	}
+}
+		
+		
+		
+		
+		
 		
 
-	}
-
-}
