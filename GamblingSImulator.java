92e6e70030b359bf9043f2420139a9b9a0d06ac7
Karthik <jeefkarthik036@gmail.com>
2022-03-31 12:00:38 +0530

    added-UC3

--- GamblingSImulator.java
+++ GamblingSImulator.java
@@ -1,37 +1,39 @@
 package com.bridge;
 
 public class GamblingSImulator {
-	private static final int Amt = 100;
-	private static final int bet =1;
-	public int stake =0;
-	
-	
+	private  int Amt = 100;
+	private  int bet = 1;
+	public int stake = 0;
+
 	public void game() {
-		double game = Math.floor(Math.random()*10)%2;
+		while (Amt > 50 && Amt < 150) {
+			double game = Math.floor(Math.random() * 10) % 2;
+
+			if (bet == game) {
+				System.out.println("won 1 dollar");
+				Amt++;
+			} else {
+				System.out.println("lost 1 dollar");
+				Amt--;
+			}
+
+		}
+			System.out.println("Amount :" + Amt);
+			
+			if(Amt == 50) {
+				System.out.println("player lost and resigned");
+			}
+			else {
+				System.out.println("player won and resigned");
+			}
 		
-		if (bet == game ) {
-			System.out.println("won 1 dollar");
-			stake++;
-		}
-		else {
-			System.out.println("lost 1 dollar");
-			stake--;
-		}
-		System.out.println("stake :"+ stake);
 	}
-		
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Gambler simulation");
 		GamblingSImulator a = new GamblingSImulator();
 		a.game();
 	}
-}
-		
-		
-		
-		
-		
-		
 
+}
