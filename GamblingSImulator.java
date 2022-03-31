0bf9d88ad00a43bfe704723731f1866072139aa2
Karthik <jeefkarthik036@gmail.com>
2022-03-31 12:14:30 +0530

    added-UC4

--- GamblingSImulator.java
+++ GamblingSImulator.java
@@ -1,32 +1,40 @@
 package com.bridge;
 
 public class GamblingSImulator {
-	private  int Amt = 100;
-	private  int bet = 1;
-	public int stake = 0;
+	private int bet = 1;
 
 	public void game() {
-		while (Amt > 50 && Amt < 150) {
-			double game = Math.floor(Math.random() * 10) % 2;
-
-			if (bet == game) {
-				System.out.println("won 1 dollar");
-				Amt++;
+		int wins = 0;
+		int loss = 0;
+		int totalAmount = 0;
+		for (int day = 1; day <= 20; day++) {
+			int stake = 100;
+			while (stake > 50 && stake < 150) {
+				double gameCheck = Math.floor(Math.random() * 10) % 2;
+				if (bet == gameCheck) {
+					// System.out.println("you won the game");
+					stake++;
+				} else {
+					// System.out.println("you lost the game");
+					stake--;
+				}
+			}
+			if (stake == 50) {
+				loss++;
+				System.out.println("Day " + day + " Lost ");
 			} else {
-				System.out.println("lost 1 dollar");
-				Amt--;
+				wins++;
+				System.out.println("Day " + day + " Won ");
 			}
-
 		}
-			System.out.println("Amount :" + Amt);
-			
-			if(Amt == 50) {
-				System.out.println("player lost and resigned");
-			}
-			else {
-				System.out.println("player won and resigned");
-			}
-		
+
+		int finalAmount = wins * 50 - loss * 50;
+		if (finalAmount > 0)
+			System.out.println("After 20 days Gambler won: $ " + finalAmount);
+		else if (finalAmount < 0)
+			System.out.println("After 20 days Gambler lost: $ " + finalAmount);
+		else
+			System.out.println("No profit or loss ");
 	}
 
 	public static void main(String[] args) {
